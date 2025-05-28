b9152aa32529aa228cecf3e5a1e58f00df192d74
kicobicn <kicobicn@163.com>
2025-05-28 18:58:45 +0800

    test

---
+++ src/main/java/com/kicobicn/KicosideaDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kicobicn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KicosideaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
